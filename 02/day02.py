287610bc00ab7cc6dd8966cc625f28ba6b709eb8
Cameron Bunce <cameronfbunce@gmail.com>
2020-12-02 08:59:56 -0500

    modified:   02/day02.py

--- 02/day02.py
+++ 02/day02.py
@@ -1,14 +1,9 @@
 with open("input") as f:
    passdb = list(f)
 
-def isvalid(rule, key, passwd):
-   passwd = passwd[0:-1]
-   key = key[0]
-   low, high = rule.split('-')
-   low = int(low)
-   high = int(high)
-
+def isvalid(parsed):
    count = 0 
+   low, high, key, passwd = parsed
    for a in passwd:
       if a == key:
          count = count + 1
@@ -17,11 +12,30 @@ def isvalid(rule, key, passwd):
    else:
       return False
 
+def rule2(parsed):
+   low, high, key, passwd = parsed
+   if (passwd[low-1] == key) ^ (passwd[high-1] == key):
+      return True
+   return False
+
+def passparser(line):
+   rule, key, passwd = line.split(' ')
+   passwd = passwd[0:-1]
+   key = key[0]
+   low, high = rule.split('-')
+   low = int(low)
+   high = int(high)
+
+   return low, high, key, passwd
+
+
 valid = 0
-
+valid2 = 0
 for line in passdb:
-   a, b, c = line.split(' ')
-   if isvalid(a, b, c):
+   parsed = passparser(line)
+   if isvalid(parsed):
       valid = valid + 1
-
+   if rule2(parsed):
+      valid2 = valid2 + 1
 print(str(valid))
+print(str(valid2))
